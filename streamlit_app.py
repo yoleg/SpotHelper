7255d7fe8c010f22557a2f2fd3e5ef14d15ae66a
Oleg Pryadko <oleg+git@olegpryadko.com>
2025-06-19 20:45:55 -0700

    fix for cloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 import logging
+from pathlib import Path
 
 import streamlit as st
 
-
+pages_root = Path(__file__).parent
 pages = [
-    st.Page(r"pages\spot_helper.py", title="Spot Helper Demo", icon="🪂"),
-    st.Page(r"pages\winds.py", title="Winds Aloft", icon="🌬️"),
-    st.Page(r"pages\dropzones.py", title="Dropzone List", icon="📃"),
-    st.Page(r"pages\canopies.py", title="Canopies List", icon="📃"),
-    st.Page(r"pages/debug.py", title="Debug", icon="🐞"),
+    st.Page(pages_root / r"pages/spot_helper.py", title="Spot Helper Demo", icon="🪂"),
+    st.Page(pages_root / r"pages/winds.py", title="Winds Aloft", icon="🌬️"),
+    st.Page(pages_root / r"pages/dropzones.py", title="Dropzone List", icon="📃"),
+    st.Page(pages_root / r"pages/canopies.py", title="Canopies List", icon="📃"),
+    st.Page(pages_root / r"pages/debug.py", title="Debug", icon="🐞"),
 ]
 nav = st.navigation(pages, position="sidebar", expanded=True)
 nav.run()
